e91787d8753379a299395820afcf645999b8fd87
Fabian Orccon <cfoch.fabian@gmail.com>
2018-05-01 21:01:30 -0500

    scripts: Copiar hook a common/ en install_hooks.sh

--- scripts/install_hooks.sh
+++ scripts/install_hooks.sh
@@ -4,6 +4,6 @@ GIT_REPO_DIR=$(git rev-parse --show-toplevel)
 PRE_COMMIT_HOOK_DST=$GIT_REPO_DIR/.git/hooks/pre-commit
 
 if [ ! -f $PRE_COMMIT_HOOK_DST ]; then
-	cp $GIT_REPO_DIR/hooks/pre-commit.sh $PRE_COMMIT_HOOK_DST
+	cp $GIT_REPO_DIR/common/hooks/pre-commit.sh $PRE_COMMIT_HOOK_DST
 	chmod +x $PRE_COMMIT_HOOK_DST
 fi
